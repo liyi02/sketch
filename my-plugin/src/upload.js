7b73c21e21b52e97d90a421fc1e306b5540a6187
zhujinyuan003 <zhujinyuan003@ke.com>
2021-01-05 20:50:24 +0800

    增加preview导出

--- my-plugin/src/upload.js
+++ my-plugin/src/upload.js
@@ -315,8 +315,10 @@ var SM = {
                     self.wantsStop = true;
                     log(e)
                 }
-
+                
+                console.log("进度：" + (layerCount / artboard.children().length))
                 if (layerIndex >= artboard.children().length) {
+                    console.log("成了！")
                     var objectID = artboard.objectID(),
                     artboardRect = self.getRect(artboard),
                     page = artboard.parentGroup(),
@@ -332,6 +334,22 @@ var SM = {
                     
                     var newData = JSON.parse(JSON.stringify(data));
                     newData.artboards = [data.artboards[artboardIndex]];
+
+                    data.artboards[artboardIndex].imagePath = "preview/" + encodeURI(slug) + ".png";
+
+                    self.exportImage({
+                            layer: artboard,
+                            path: self.toJSString(savePath) + "/preview",
+                            scale: 2,
+                            // name: objectID,
+                            name: slug
+                        });
+
+                    self.writeFile({
+                            content: "<meta http-equiv=\"refresh\" content=\"0;url=../index.html#artboard" + artboardIndex + "\">",
+                            path: self.toJSString(savePath) + "/links",
+                            fileName: slug + ".html"
+                        });
 
                     layerIndex = 0;
                     artboardIndex++;
@@ -398,27 +416,23 @@ TextAligns = ["left", "right", "center", "justify", "left"],
 ResizingType = ["stretch", "corner", "resize", "float"];
 
 // api.js
-
 SM.extend({
     is: function(layer, theClass){
         if(!layer) return false;
         var klass = layer.class();
         return klass === theClass;
     },
-    toJSNumber: function(str){
-        return Number( this.toJSString(str) );
+    addGroup: function(){
+        return MSLayerGroup.new();
     },
-    toJSString: function(str){
-        return new String(str).toString();
+    addShape: function(){
+		return MSShapeGroup.shapeWithRect(NSMakeRect(0, 0, 100, 100));
+
     },
-    toSlug: function(str){
-        return this.toJSString(str)
-                .toLowerCase()
-                .replace(/(<([^>]+)>)/ig, "")
-                .replace(/[\/\+\|]/g, " ")
-                .replace(new RegExp("[\\!@#$%^&\\*\\(\\)\\?=\\{\\}\\[\\]\\\\\\\,\\.\\:\\;\\']", "gi"),'')
-                .replace(/\s+/g,'-')
-                ;
+    addText: function(container){
+        var text = MSTextLayer.new();
+        text.setStringValue("text");
+        return text;
     },
     removeLayer: function(layer){
         var container = layer.parentGroup();
@@ -439,14 +453,83 @@ SM.extend({
             setHeight: function(height){ rect.setHeight(height); this.height = height; this.maxY = this.y + this.height; }
         };
     },
+    toNopPath: function(str){
+        return this.toJSString(str).replace(/[\/\\\?]/g, " ");
+    },
+    toHTMLEncode: function(str){
+        return this.toJSString(str)
+                    .replace(/\</g, "&lt;")
+                    .replace(/\>/g, '&gt;')
+                    .replace(/\'/g, "&#39;")
+                    .replace(/\"/g, "&quot;")
+                    .replace(/\u2028/g,"\\u2028")
+                    .replace(/\u2029/g,"\\u2029")
+                    .replace(/\ud83c|\ud83d/g,"")
+                ;
+        // return str.replace(/\&/g, "&amp;").replace(/\"/g, "&quot;").replace(/\'/g, "&#39;").replace(/\</g, "&lt;").replace(/\>/g, '&gt;');
+    },
+    emojiToEntities: function(str) {
+      var self = this,
+          emojiRegExp = new RegExp("(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])", "g");
+        return str.replace(
+              emojiRegExp,
+              function(match) {
+                  var u = "";
+                  for (var i = 0; i < match.length; i++) {
+                      if( !(i%2) ){
+                        u += "&#" + match.codePointAt(i)
+                      }
+                  }
+
+                  return u;
+              });
+    },
+    toSlug: function(str){
+        return this.toJSString(str)
+                .toLowerCase()
+                .replace(/(<([^>]+)>)/ig, "")
+                .replace(/[\/\+\|]/g, " ")
+                .replace(new RegExp("[\\!@#$%^&\\*\\(\\)\\?=\\{\\}\\[\\]\\\\\\\,\\.\\:\\;\\']", "gi"),'')
+                .replace(/\s+/g,'-')
+                ;
+    },
+    toJSString: function(str){
+        return new String(str).toString();
+    },
+    toJSNumber: function(str){
+        return Number( this.toJSString(str) );
+    },
+    pointToJSON: function(point){
+        return {
+            x: parseFloat(point.x),
+            y: parseFloat(point.y)
+        };
+    },
+    rectToJSON: function(rect, referenceRect) {
+        if (referenceRect) {
+            return {
+                x: Math.round( ( rect.x() - referenceRect.x() ) * 10 ) / 10,
+                y: Math.round( ( rect.y() - referenceRect.y() ) * 10 ) / 10,
+                width: Math.round( rect.width() * 10 ) / 10,
+                height: Math.round( rect.height() * 10 ) / 10
+            };
+        }
+
+        return {
+            x: Math.round( rect.x() * 10 ) / 10,
+            y: Math.round( rect.y() * 10 ) / 10,
+            width: Math.round( rect.width() * 10 ) / 10,
+            height: Math.round( rect.height() * 10 ) / 10
+        };
+    },
     colorToJSON: function(color) {
         return {
             r: Math.round(color.red() * 255),
             g: Math.round(color.green() * 255),
             b: Math.round(color.blue() * 255),
             a: color.alpha(),
-            "color-hex": color.immutableModelObject + " " + Math.round(color.alpha() * 100) + "%",
-            "argb-hex": "#" + this.toHex(color.alpha() * 255) + color.immutableModelObject,
+            "color-hex": color.immutableModelObject().stringValueWithAlpha(false) + " " + Math.round(color.alpha() * 100) + "%",
+            "argb-hex": "#" + this.toHex(color.alpha() * 255) + color.immutableModelObject().stringValueWithAlpha(false).replace("#", ""),
             "css-rgba": "rgba(" + [
                             Math.round(color.red() * 255),
                             Math.round(color.green() * 255),
@@ -460,6 +543,45 @@ SM.extend({
                             "a:" + (Math.round(color.alpha() * 100) / 100).toFixed(2)
                         ].join(" ") + ")"
         };
+    },
+    colorStopToJSON: function(colorStop) {
+        return {
+            color: this.colorToJSON(colorStop.color()),
+            position: colorStop.position()
+        };
+    },
+    gradientToJSON: function(gradient) {
+        var stopsData = [],
+            stop, stopIter = gradient.stops().objectEnumerator();
+        while (stop = stopIter.nextObject()) {
+            stopsData.push(this.colorStopToJSON(stop));
+        }
+
+        return {
+            type: GradientTypes[gradient.gradientType()],
+            from: this.pointToJSON(gradient.from()),
+            to: this.pointToJSON(gradient.to()),
+            colorStops: stopsData
+        };
+    },
+    shadowToJSON: function(shadow) {
+        return {
+            type: shadow instanceof MSStyleShadow ? "outer" : "inner",
+            offsetX: shadow.offsetX(),
+            offsetY: shadow.offsetY(),
+            blurRadius: shadow.blurRadius(),
+            spread: shadow.spread(),
+            color: this.colorToJSON(shadow.color())
+        };
+    },
+    getRadius: function(layer){
+        if(layer.layers && this.is(layer.layers().firstObject(), MSRectangleShape)){
+            return (layer.layers().firstObject().cornerRadiusString().split(';').map(Number).length == 1) ? layer.layers().firstObject().fixedRadius() : layer.layers().firstObject().cornerRadiusString().split(';').map(Number);
+        } else if(this.is(layer, MSRectangleShape)) {
+            return (layer.cornerRadiusString().split(';').map(Number).length == 1) ? layer.fixedRadius() : layer.cornerRadiusString().split(';').map(Number);
+        } else {
+            return 0;
+        }
     },
     getBorders: function(style) {
         var bordersData = [],
@@ -491,15 +613,6 @@ SM.extend({
         }
 
         return bordersData;
-    },
-    getRadius: function(layer){
-        if(layer.layers && this.is(layer.layers().firstObject(), MSRectangleShape)){
-            return (layer.layers().firstObject().cornerRadiusString().split(';').map(Number).length == 1) ? layer.layers().firstObject().fixedRadius() : layer.layers().firstObject().cornerRadiusString().split(';').map(Number);
-        } else if(this.is(layer, MSRectangleShape)) {
-            return (layer.cornerRadiusString().split(';').map(Number).length == 1) ? layer.fixedRadius() : layer.cornerRadiusString().split(';').map(Number);
-        } else {
-            return 0;
-        }
     },
     getFills: function(style) {
         var fillsData = [],
@@ -566,9 +679,201 @@ SM.extend({
         if(!style) return "";
         return this.toJSString(style.name());
     },
+    updateContext: function(){
+        this.context.document = NSDocumentController.sharedDocumentController().currentDocument();
+        this.context.selection = this.context.document.selectedLayers().layers();
+
+        return this.context;
+    }
+});
+
+// help.js
+SM.extend({
+    mathHalf: function(number){
+        return Math.round( number / 2 );
+    },
+    convertUnit: function(length, isText, percentageType){
+        if(length.length){
+            var units = this.configs.unit.split("/"),
+                unit = units[0];
+
+            if( units.length > 1 && isText){
+                unit = units[1];
+            }
+
+            var scale = this.configs.scale;
+            var tempLegth = [];
+
+            length.forEach(function(element) {
+                tempLegth.push(Math.round( element / scale * 10 ) / 10);
+            });
+
+            return tempLegth.join(unit + ' ') + unit;
+
+        } else {
+
+            if(percentageType && this.artboard){
+                var artboardRect = this.getRect( this.artboard );
+                if (percentageType == "width") {
+                     return Math.round((length / artboardRect.width) * 1000) / 10 + "%";
+                }
+                else if(percentageType == "height"){
+                    return Math.round((length / artboardRect.height) * 1000) / 10 + "%";
+                }
+            }
+
+            var length = Math.round( length / this.configs.scale * 10 ) / 10,
+                units = this.configs.unit.split("/"),
+                unit = units[0];
+
+            if( units.length > 1 && isText){
+                unit = units[1];
+            }
+
+            return length + unit;
+        }
+
+    },
     toHex:function(c) {
         var hex = Math.round(c).toString(16).toUpperCase();
         return hex.length == 1 ? "0" + hex :hex;
+    },
+    hexToRgb:function(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: this.toHex(result[1]),
+            g: this.toHex(result[2]),
+            b: this.toHex(result[3])
+        } : null;
+    },
+    isIntersect: function(targetRect, layerRect){
+        return !(
+            targetRect.maxX <= layerRect.x ||
+            targetRect.x >= layerRect.maxX ||
+            targetRect.y >= layerRect.maxY ||
+            targetRect.maxY <= layerRect.y
+        );
+    },
+    getDistance: function(targetRect, containerRect){
+        var containerRect = containerRect || this.getRect(this.current);
+
+        return {
+            top: (targetRect.y - containerRect.y),
+            right: (containerRect.maxX - targetRect.maxX),
+            bottom: (containerRect.maxY - targetRect.maxY),
+            left: (targetRect.x - containerRect.x),
+        }
+    },
+    message: function(message){
+        this.document.showMessage(message);
+    },
+    find: function(format, container, returnArray){
+        if(!format || !format.key  || !format.match){
+            return false;
+        }
+        var predicate = NSPredicate.predicateWithFormat(format.key,format.match),
+            container = container || this.current,
+            items;
+
+        if(container.pages){
+            items = container.pages();
+        }
+        else if( this.is( container, MSSharedStyleContainer ) || this.is( container, MSSharedTextStyleContainer ) ){
+            items = container.objectsSortedByName();
+        }
+        else if( container.children ){
+            items = container.children();
+        }
+        else{
+            items = container;
+        }
+
+        var queryResult = items.filteredArrayUsingPredicate(predicate);
+
+        if(returnArray) return queryResult;
+
+        if (queryResult.count() == 1){
+            return queryResult[0];
+        } else if (queryResult.count() > 0){
+            return queryResult;
+        } else {
+            return false;
+        }
+    },
+    clearAllMarks: function(){
+        var layers = this.page.children().objectEnumerator();
+        while(layer = layers.nextObject()) {
+            if(this.is(layer, MSLayerGroup) && this.regexNames.exec(layer.name())){
+                this.removeLayer(layer)
+            }
+        }
+    },
+    toggleHidden: function(){
+        var isHidden = (this.configs.isHidden)? false : !Boolean(this.configs.isHidden);
+        this.configs = this.setConfigs({isHidden: isHidden});
+
+        var layers = this.page.children().objectEnumerator();
+
+        while(layer = layers.nextObject()) {
+            if(this.is(layer, MSLayerGroup) && this.regexNames.exec(layer.name())){
+                layer.setIsVisible(!isHidden);
+            }
+        }
+    },
+    toggleLocked: function(){
+        var isLocked = (this.configs.isLocked)? false : !Boolean(this.configs.isLocked);
+        this.configs = this.setConfigs({isLocked: isLocked});
+
+        var layers = this.page.children().objectEnumerator();
+
+        while(layer = layers.nextObject()) {
+            if(this.is(layer, MSLayerGroup) && this.regexNames.exec(layer.name())){
+                layer.setIsLocked(isLocked);
+            }
+        }
+    },
+});
+
+// export.js
+SM.extend({
+    hasEmoji: function(layer) {
+        var fonts = layer.attributedString().fontNames().allObjects();
+        return !!/AppleColorEmoji/.exec(fonts);
+    },
+    getExportable: function(layer, savePath){
+        var self = this,
+            exportable = [],
+            size, sizes = layer.exportOptions().exportFormats(),
+            fileFormat = this.toJSString(sizes[0].fileFormat()),
+            matchFormat = /png|jpg|tiff|webp/.exec(fileFormat);
+        var exportFormats =
+        [
+            { scale: 1, suffix: "", format: fileFormat },
+            { scale: 2, suffix: "@2x", format: fileFormat },
+            { scale: 3, suffix: "@3x", format: fileFormat }
+        ];
+
+        for(var exportFormat of exportFormats) {
+          var prefix = exportFormat.prefix || "",
+              suffix = exportFormat.suffix || "";
+          self.exportImage({
+                  layer: layer,
+                  path: self.assetsPath,
+                  scale: exportFormat.scale,
+                  name: layer.name(),
+                  prefix: prefix,
+                  suffix: suffix,
+                  format: exportFormat.format
+              });
+
+          exportable.push({
+                  name: self.toJSString(layer.name()),
+                  format: fileFormat,
+                  path: prefix + layer.name() + suffix + "." + exportFormat.format
+              });
+        }
+
+        return exportable;
     },
     getSymbol: function(artboard, layer, layerData, data){
         if( layerData.type == "symbol" ){
@@ -589,7 +894,8 @@ SM.extend({
 
                 overrides = (overrides)? overrides.objectForKey(0): undefined;
 
-                while(tempSymbolLayer = tempSymbolLayers.nextObject()){
+                var tempSymbolLayer = tempSymbolLayers.nextObject()
+                while(tempSymbolLayer){
                     if( self.is(tempSymbolLayer, MSSymbolInstance) ){
                         var symbolMasterObjectID = self.toJSString(symbolChildren[idx].objectID());
                         if(
@@ -614,14 +920,28 @@ SM.extend({
                           symbolChildren[idx]
                       );
                     }
+                    tempSymbolLayer = tempSymbolLayers.nextObject()
                     idx++
                 }
                 this.removeLayer(tempGroup);
             }
         }
     },
-});
+    getTextAttrs: function(str){
+        var data = {},
+            regExpAttr = new RegExp('([a-z\-]+)\=\"([^\"]+)\"', 'g'),
+            regExpAttr1 = new RegExp('([a-z\-]+)\=\"([^\"]+)\"'),
+            attrs = str.match(regExpAttr);
+        for (var a = 0; a < attrs.length; a++) {
+            var attrData = regExpAttr1.exec(attrs[a]),
+                key = attrData[1],
+                value = attrData[2];
 
+            data[key] = value;
+        }
+        return data;
+    }
+})
 
 // getlayer
 SM.extend({
@@ -749,6 +1069,38 @@ SM.extend({
             isEmpty: isEmpty,
             isShapeGroup: isShapeGroup
         }
+    },
+    exportImage: function(options) {
+        var options = this.extend(options, {
+                layer: this.artboard,
+                path: this.toJSString(NSTemporaryDirectory()),
+                scale: 1,
+                name: "preview",
+                prefix: "",
+                suffix: "",
+                format: "png"
+            }),
+            document = this.document,
+            slice = MSExportRequest.exportRequestsFromExportableLayer(options.layer).firstObject(),
+            savePathName = [];
+
+        slice.scale = options.scale;
+        slice.format = options.format;
+
+        savePathName.push(
+                options.path,
+                "/",
+                options.prefix,
+                options.name,
+                options.suffix,
+                ".",
+                options.format
+            );
+        savePathName = savePathName.join("");
+
+        document.saveArtboardOrSlice_toFile(slice, savePathName);
+
+        return savePathName;
     },
     getLayer: function (artboard, layer, data, symbolLayer) {
         var artboardRect = artboard.absoluteRect(),
